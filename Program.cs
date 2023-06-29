34a94409ed25dfce0ded19ad1d3a1ebe7c0e2c6e
Ilya <ok.brain@yandex.ru>
2023-06-29 21:32:59 +0300

    Create Programm

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] arraySum = new string [5] { "123", "1567", "-2", "computer science", "Hey"};
+
+string [] arrayResult = new string[arraySum.Length];
+
+
+    string[] arrayFill(string[] arraySum, string[] arrayResult) {
+
+    int temp = 0;
+
+    for (int i = 0; i <arraySum.Length; i++)
+    {
+        if(arraySum[i].Length <=3) {
+
+            arrayResult[temp] = arraySum[i];
+            temp++;
+        }        
+    }
+    return arrayResult;
+}
+
+void arrayPrint (string [] array) {
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+arrayResult = arrayFill(arraySum, arrayResult);
+
+arrayPrint(arrayResult);
